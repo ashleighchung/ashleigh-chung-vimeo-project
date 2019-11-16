e3c33496b7ff76f3bb2f3a99452252ecfb59e8cd
ashleighchung <toto@TOTOs-MacBook-Pro-2.local>
2019-11-16 10:10:23 -0500

    Integrated flex-box

--- src/Monsoon/index.js
+++ src/Monsoon/index.js
@@ -5,6 +5,25 @@ import Image from './monsoon.jpg';
 function monsoon() {
   return (
     <div className="background monsoon-background">
+        <img className = "monsoon-image" src = {Image} alt = "monsoon" />
+        <div className = "text">
+            <text className = "monsoon-title">MONSOON III</text>
+            <p className = "paragraph monsoon-paragraph">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed non vestibulum lacus. Mauris et interdum lectus. Aenean a erat at metus ultrices cursus et non quam. Morbi egestas euismod sapien et ultrices. Sed ac massa condimentum, suscipit mi vitae, facilisis turpis. </p>
+        </div>
+    </div>
+  );
+}
+
+export default monsoon;
+
+
+/*import React from 'react';
+import './monsoon.scss';
+import Image from './monsoon.jpg';
+
+function monsoon() {
+  return (
+    <div className="background monsoon-background">
       <img className = "monsoon-image" src = {Image} alt = "monsoon" />
       <div className = "text">
           <text className = "monsoon-title">MONSOON III</text>
@@ -15,3 +34,4 @@ function monsoon() {
 }
 
 export default monsoon;
+*/
