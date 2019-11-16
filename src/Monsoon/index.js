f5cc04b7c752feadd980d22208661ee214e6b7a2
ashleighchung <toto@TOTOs-MacBook-Pro-2.local>
2019-11-16 11:37:34 -0500

    Organized CSS and prevented resizing when window resizes

--- src/Monsoon/index.js
+++ src/Monsoon/index.js
@@ -5,9 +5,9 @@ import Image from './monsoon.jpg';
 function monsoon() {
   return (
     <div className="background monsoon-background">
-        <img className = "monsoon-image" src = {Image} alt = "monsoon" />
+        <img className = "image" src = {Image} alt = "monsoon" />
         <div className = "text">
-            <text className = "monsoon-title">MONSOON III</text>
+            <text className = "title">MONSOON III</text>
             <p className = "paragraph monsoon-paragraph">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed non vestibulum lacus. Mauris et interdum lectus. Aenean a erat at metus ultrices cursus et non quam. Morbi egestas euismod sapien et ultrices. Sed ac massa condimentum, suscipit mi vitae, facilisis turpis. </p>
         </div>
     </div>
